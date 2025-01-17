670dc69b29dbb474deff7abbe4fefa30665faedd
James Alexander <86769868+jamesleealexander@users.noreply.github.com>
2025-01-17 13:56:07 +0000

    Add revert method and return results as an object

--- src/DecimalTime.php
+++ src/DecimalTime.php
@@ -4,8 +4,29 @@ namespace Jamesleealexander\DecimalTime;
 
 class DecimalTime
 {
-    public static function convert(int $hours = 0, int $minutes = 0, int $seconds = 0): float
+    public static function convert(int $hours = 0, int $minutes = 0, int $seconds = 0): object
     {
-        return $hours + ($minutes / 60) + ($seconds / 3600);
+        return (object) [
+            'decimal' => $hours + ($minutes / 60) + ($seconds / 3600),
+            'hours' => $hours,
+            'minutes' => $minutes,
+            'seconds' => $seconds,
+        ];
+    }
+
+    public static function revert(float $decimal = 0.0): object
+    {
+        $seconds = $decimal * 3600;
+        $hours = floor($decimal);
+        $seconds -= $hours * 3600;
+        $minutes = floor($seconds / 60);
+        $seconds -= $minutes * 60;
+
+        return (object) [
+            'decimal' => $decimal,
+            'hours' => $hours,
+            'minutes' => $minutes,
+            'seconds' => $seconds,
+        ];
     }
 }
